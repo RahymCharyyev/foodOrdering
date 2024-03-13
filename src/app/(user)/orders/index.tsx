291c5b4a582b245eedc06b0f1d3752c54948b3ca
RahymCharyyev <r569837@gmail.com>
2024-03-13 17:45:46 +0500

    ✨feature: sign in and sign up

---
+++ src/app/(user)/orders/index.tsx
@@ -0,0 +1,12 @@
+import orders from '@/assets/data/orders';
+import OrderListItem from '@/src/components/OrderListItem';
+import { FlatList } from 'react-native';
+
+export default function OrderScreen() {
+  return (
+    <FlatList
+      data={orders}
+      renderItem={({ item }) => <OrderListItem order={item} />}
+    />
+  );
+}
